29c4c6b5b78c1c6b09e27c5cf5cbef410d14af2b
summer <wpsumsun@126.com>
2018-04-24 13:20:58 +0800

    [Feature]前端准备

--- src/js/app/index.js
+++ src/js/app/index.js
@@ -1,3 +1,15 @@
-// var $ = require('../library/jquery.min')
-var waterFull = require('../module/waterFull')
-waterFull.init($('.wrap'))
+require('less/index.less');
+
+var NoteManager = require('mod/note-manager.js').NoteManager;
+var Event = require('mod/event.js');
+var WaterFall = require('mod/waterfall.js');
+
+NoteManager.load();
+
+$('.add-note').on('click', function() {
+  NoteManager.add();
+})
+
+Event.on('waterfall', function(){
+  WaterFall.init($('#content'));
+})
--- src/webpack.config.js
+++ src/webpack.config.js
@@ -4,12 +4,14 @@ var path = require('path')
 module.exports = {
     entry: path.join(__dirname, 'js/app/index.js'),
     output: {
-        path: path.join(__dirname, '../public/javascripts'),
+        path: path.join(__dirname, '../public/js'),
         filename: 'index.js'
     },
     resolve: {
         alias: {
-            jquery: path.resolve(__dirname, 'js/library/jquery.min.js')
+            jquery: path.resolve(__dirname, 'js/library/jquery.min.js'),
+            mod: path.resolve(__dirname, 'js/module'),
+            less: path.resolve(__dirname, 'less')
         }
     },
     module: {
